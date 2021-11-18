f3d1db56855eb40acc75a4f1ba0cbd3180d22305
Donggeun Lee <patlee997@gmail.com>
2021-11-18 16:14:44 +0900

    Extension: UIView added

---
+++ WhoMI_Demo/Extension/UIView+Extension.swift
@@ -0,0 +1,18 @@
+//
+//  UIView+Extension.swift
+//  WhoMI_Demo
+//
+//  Created by Donggeun Lee on 2021/11/18.
+//
+
+import UIKit
+
+extension UIView {
+    @IBInspectable var cornerRadius: CGFloat {
+        get { return cornerRadius }
+        set {
+            self.layer.cornerRadius = newValue
+        }
+    }
+}
+
