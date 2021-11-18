43ddcd6011188b7e32a1589591ef189005352bb9
Donggeun Lee <patlee997@gmail.com>
2021-11-18 16:16:00 +0900

    ChangePNViewController added

---
+++ WhoMI_Demo/ViewControllers/ChangePNViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ChangePNViewController.swift
+//  WhoMI_Demo
+//
+//  Created by Donggeun Lee on 2021/11/17.
+//
+
+import UIKit
+
+class ChangePNViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
