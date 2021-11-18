2b2b3c57ec545dff5a433607857fd87f97fc248c
Donggeun Lee <patlee997@gmail.com>
2021-11-18 16:15:03 +0900

    OnboardingSlide added

---
+++ WhoMI_Demo/OnboardingSlide.swift
@@ -0,0 +1,14 @@
+//
+//  OnboardingSlide.swift
+//  WhoMI_Demo
+//
+//  Created by Donggeun Lee on 2021/11/18.
+//
+
+import UIKit
+
+struct OnboardingSlide {
+    let title: String
+    let description: String
+    let image: UIImage
+}
