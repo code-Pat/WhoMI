a3fb38f4f4987e3c02f46155fa79b7f814b8140b
Donggeun Lee <patlee997@gmail.com>
2021-11-18 16:16:50 +0900

    OnboardingViewController added

---
+++ WhoMI_Demo/ViewControllers/OnboardingViewController.swift
@@ -0,0 +1,86 @@
+//
+//  OnboardingViewController.swift
+//  WhoMI_Demo
+//
+//  Created by Donggeun Lee on 2021/11/17.
+//
+
+import UIKit
+
+class OnboardingViewController: UIViewController {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var nextButton: UIButton!
+    @IBOutlet weak var pageControl: UIPageControl!
+    
+    var slides: [OnboardingSlide] = []
+    
+    var currentPage = 0 {
+        didSet {
+            pageControl.currentPage = currentPage
+            if currentPage == slides.count - 1 {
+                nextButton.setTitle("Get Started", for: .normal)
+                
+            } else {
+                nextButton.setTitle("Next", for: .normal)
+            }
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        slides = [
+            OnboardingSlide(title: "일단 임시 제목", description: "일단 임시 설명", image: UIImage(named: "sampleImage")!),
+            OnboardingSlide(title: "일단 임시 제목2", description: "일단 임시 설명2", image: UIImage(named: "sampleImage")!),
+            OnboardingSlide(title: "일단 임시 제목3", description: "일단 임시 설명3", image: UIImage(named: "sampleImage")!)
+        ]
+        
+    }
+    
+    @IBAction func nextBtnClicked(_ sender: UIButton) {
+        if currentPage == slides.count - 1 {
+            let controller = storyboard?.instantiateViewController(withIdentifier: "homeNC") as! UINavigationController
+            controller.modalPresentationStyle = .fullScreen
+            controller.modalTransitionStyle = .flipHorizontal
+            present(controller, animated: true, completion: nil)
+        } else {
+            currentPage += 1
+            let indexPath = IndexPath(item: currentPage, section: 0)
+            collectionView.scrollToItem(at: indexPath, at: .centeredHorizontally, animated: true)
+        }
+        
+        
+    }
+    
+}
+
+extension OnboardingViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return slides.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: OnboardingCell.identifier, for: indexPath) as! OnboardingCell
+        
+        cell.setup(slides[indexPath.row])
+        
+        return cell
+    }
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        let width = scrollView.frame.width
+        currentPage = Int(scrollView.contentOffset.x / width)
+        
+    }
+}
+
+extension OnboardingViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        return CGSize(width: collectionView.frame.width, height: collectionView.frame.height)
+    }
+}
