a5c4b6edda8ad56eff8e7e28c2d1e7d090c1ef45
Donggeun Lee <patlee997@gmail.com>
2021-11-19 14:57:44 +0900

    connected with storyboard

--- WhoMI_Demo/ViewControllers/LogInViewController.swift
+++ WhoMI_Demo/ViewControllers/LogInViewController.swift
@@ -9,21 +9,18 @@ import UIKit
 
 class LogInViewController: UIViewController {
 
+    @IBOutlet weak var idTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var logInButton: UIButton!
+    @IBOutlet weak var errorLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func logInBtnClicked(_ sender: UIButton) {
     }
-    */
+    
 
 }
