535ca7b020b21f1b67c6ac691f0d26f172a9f11c
Donggeun Lee <patlee997@gmail.com>
2021-11-19 15:56:01 +0900

    SignUpViewController updated

--- WhoMI_Demo/ViewControllers/SignUpViewController.swift
+++ WhoMI_Demo/ViewControllers/SignUpViewController.swift
@@ -19,9 +19,25 @@ class SignUpViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        setUpElements()
         
     }
     
     @IBAction func signUpBtnClicked(_ sender: UIButton) {
+        
+    }
+    
+    func setUpElements() {
+        
+        // 에러메세지 숨김
+        errorLabel.alpha = 0
+        
+        // elements 스타일
+        Utilities.styleTextField(firstNameTextField)
+        Utilities.styleTextField(lastNameTextField)
+        Utilities.styleTextField(emailTextField)
+        Utilities.styleTextField(passwordTextField)
+        Utilities.styleFilledButton(signUpButton)
+        
     }
 }
