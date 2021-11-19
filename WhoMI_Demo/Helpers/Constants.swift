0e5b509a58fec60c1edcc4cf967013cc940ddfba
Donggeun Lee <patlee997@gmail.com>
2021-11-19 21:04:02 +0900

    Constants added

---
+++ WhoMI_Demo/Helpers/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  WhoMI_Demo
+//
+//  Created by Donggeun Lee on 2021/11/19.
+//
+
+import Foundation
+
+struct Constants {
+    
+    struct Storyboard {
+        
+        static let homeNavigationController = "homeNC"
+        static let homeViewController = "homeVC"
+    }
+}
