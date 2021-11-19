87fae14588de690c42f7f74e24dec70d5cadd9c3
Donggeun Lee <patlee997@gmail.com>
2021-11-19 14:57:54 +0900

    connected with storyboard

--- WhoMI_Demo/ViewControllers/SignUpViewController.swift
+++ WhoMI_Demo/ViewControllers/SignUpViewController.swift
@@ -9,10 +9,19 @@ import UIKit
 
 class SignUpViewController: UIViewController {
 
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var signUpButton: UIButton!
+    @IBOutlet weak var errorLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         
     }
     
+    @IBAction func signUpBtnClicked(_ sender: UIButton) {
+    }
 }
