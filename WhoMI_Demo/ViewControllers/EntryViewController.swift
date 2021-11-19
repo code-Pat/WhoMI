508383e52b3220e161ba63ae989505d3d6aa7229
Donggeun Lee <patlee997@gmail.com>
2021-11-19 15:55:42 +0900

    EntryViewController updated

--- WhoMI_Demo/ViewControllers/EntryViewController.swift
+++ WhoMI_Demo/ViewControllers/EntryViewController.swift
@@ -9,21 +9,21 @@ import UIKit
 
 class EntryViewController: UIViewController {
 
+    @IBOutlet weak var signUpButton: UIButton!
+    @IBOutlet weak var logInButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setUpElements()
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setUpElements() {
+        
+        Utilities.styleFilledButton(signUpButton)
+        Utilities.styleHollowButton(logInButton)
     }
-    */
+    
 
 }
