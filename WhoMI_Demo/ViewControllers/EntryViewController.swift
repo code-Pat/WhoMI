47c37b673407ef1b767a260b5196142113efb5f9
Donggeun Lee <patlee997@gmail.com>
2021-11-19 14:44:44 +0900

    storyboard updated: - sign up View - log in view - entry view

---
+++ WhoMI_Demo/ViewControllers/EntryViewController.swift
@@ -0,0 +1,29 @@
+//
+//  EntryViewController.swift
+//  WhoMI_Demo
+//
+//  Created by Donggeun Lee on 2021/11/19.
+//
+
+import UIKit
+
+class EntryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
