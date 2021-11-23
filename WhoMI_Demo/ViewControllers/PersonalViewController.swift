7451fa05cae775e2ecf0e052594d645f28d0ae93
Donggeun Lee <patlee997@gmail.com>
2021-11-23 21:29:23 +0900

    PersonalViewController - connected with storyboard

--- WhoMI_Demo/ViewControllers/PersonalViewController.swift
+++ WhoMI_Demo/ViewControllers/PersonalViewController.swift
@@ -8,7 +8,56 @@
 import UIKit
 
 class PersonalViewController: UIViewController {
-
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var entireView: UIView!
+    //topView
+    @IBOutlet weak var topView: UIView!
+    @IBOutlet weak var settingButton: UIButton!
+    @IBOutlet weak var editButton: UIButton!
+    //stackView
+    @IBOutlet weak var stackView: UIStackView!
+    //pictureView
+    @IBOutlet weak var pictureView: UIView!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var uploadButton: UIButton!
+    //labelView
+    @IBOutlet weak var labelView: UIView!
+    @IBOutlet weak var sectionLabel: UILabel!
+    //nameView
+    @IBOutlet weak var nameView: UIView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var nameCountLabel: UILabel!
+    @IBOutlet weak var nameTextField: UITextField!
+    //birthView
+    @IBOutlet weak var birthView: UIView!
+    @IBOutlet weak var birthLabel: UILabel!
+    @IBOutlet weak var birthCountLabel: UILabel!
+    @IBOutlet weak var birthTextField: UITextField!
+    //genderView
+    @IBOutlet weak var genderView: UIView!
+    @IBOutlet weak var genderLabel: UILabel!
+    @IBOutlet weak var genderCountLabel: UILabel!
+    @IBOutlet weak var genderTextField: UITextField!
+    //phoneNumberView
+    @IBOutlet weak var phoneNumberView: UIView!
+    @IBOutlet weak var phoneNumberLabel: UILabel!
+    @IBOutlet weak var phoneCountLabel: UILabel!
+    @IBOutlet weak var phoneTextField: UITextField!
+    //emailView
+    @IBOutlet weak var emailView: UIView!
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var emailCountLabel: UILabel!
+    @IBOutlet weak var emailTextField: UITextField!
+    //addressView
+    @IBOutlet weak var addressView: UIView!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var addressCountLabel: UILabel!
+    @IBOutlet weak var addressTextField: UITextField!
+    //nextView
+    @IBOutlet weak var nextView: UIView!
+    @IBOutlet weak var nextButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
