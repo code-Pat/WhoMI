c7b4546a0164f1ef360a7b52c73592796d7ccf7a
Donggeun Lee <patlee997@gmail.com>
2021-11-23 21:55:18 +0900

    Personal3ViewController - connected with storyboard

---
+++ WhoMI_Demo/ViewControllers/Personal3ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  Personal3ViewController.swift
+//  WhoMI_Demo
+//
+//  Created by Donggeun Lee on 2021/11/23.
+//
+
+import UIKit
+
+class Personal3ViewController: UIViewController {
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var entireView: UIView!
+    //stackView
+    @IBOutlet weak var stackView: UIStackView!
+    //sectionView
+    @IBOutlet weak var sectionView: UIView!
+    @IBOutlet weak var sectionLabel: UILabel!
+    //statusView
+    @IBOutlet weak var statusView: UIView!
+    @IBOutlet weak var statusLabel: UILabel!
+    @IBOutlet weak var statusCountLabel: UILabel!
+    @IBOutlet weak var statusTextField: UITextField!
+    //introduceView
+    @IBOutlet weak var introduceView: UIView!
+    @IBOutlet weak var introduceLabel: UILabel!
+    @IBOutlet weak var introduceTextView: UITextView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
