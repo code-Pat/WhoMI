b56490a22347384a670f6344991e3a2a082dfe78
Donggeun Lee <patlee997@gmail.com>
2021-12-29 20:14:47 +0900

    AddViewController added

---
+++ WhoMI_Demo/ViewControllers/AddViewController.swift
@@ -0,0 +1,17 @@
+//
+//  AddViewController.swift
+//  WhoMI_Demo
+//
+//  Created by Donggeun Lee on 2021/12/29.
+//
+
+import UIKit
+
+class AddViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
